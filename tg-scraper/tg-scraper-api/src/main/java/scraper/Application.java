c22776a542ac37403859f52e0d8e58c99c321bd0
gviktor-f <vitek-mail@list.ru>
2024-04-17 03:42:42 +0300

    tg scraper

--- tg-scraper/tg-scraper-api/src/main/java/scraper/Application.java
+++ tg-scraper/tg-scraper-api/src/main/java/scraper/Application.java
@@ -10,18 +10,5 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-
-//        JerseyLoggingStarter.start();
-
-//        final AnnotationConfigApplicationContext ctx =
-//                new AnnotationConfigApplicationContext(SearcherConfiguration.class);
-//        ctx.registerShutdownHook();
-//
-//        final ResourceConfig resourceConfig = new BaseApiJerseyConfig(List.of("searcher"))
-//                .property("contextConfig", ctx)
-//                .setApplicationName("audio-search")
-//                .registerClasses(ApiResource.class);
-//
-//        ServerStarter.startServerOrFail(resourceConfig);
     }
 }
